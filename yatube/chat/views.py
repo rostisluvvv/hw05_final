54fbabe25b630d744d7770e2047927f600e547a0
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-09 16:56:15 +0300

    add login_required

--- yatube/chat/views.py
+++ yatube/chat/views.py
@@ -2,10 +2,12 @@ import json
 
 from django.core import serializers
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 from .models import Message
 
 
+@login_required
 def room(request):
     messages = Message.objects.all()
     data = serializers.serialize("json", messages)
