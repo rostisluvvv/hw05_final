b14625c098d2df0d05bb2d89366d295351b293de
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-09 16:52:04 +0300

    add room views

--- yatube/chat/views.py
+++ yatube/chat/views.py
@@ -1,3 +1,16 @@
+import json
+
+from django.core import serializers
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Message
+
+
+def room(request):
+    messages = Message.objects.all()
+    data = serializers.serialize("json", messages)
+
+    context = {
+        'messages': data,
+    }
+    return render(request, 'chat/room.html', context)
