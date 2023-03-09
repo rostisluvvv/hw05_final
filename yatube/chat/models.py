140b145f7b70f2f040c77274d43a7a6eee92b368
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-09 16:51:19 +0300

    add Message model

--- yatube/chat/models.py
+++ yatube/chat/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    message = models.TextField()
+    username = models.CharField(max_length=10)
+    pub_date = models.DateTimeField(auto_now_add=True)
